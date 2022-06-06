9e66fdceb9290c9d78cf1e1ee2b685587fb0f963
IFarida <98031733+IFarida@users.noreply.github.com>
2022-06-06 20:25:05 +0300

    bloc

---
+++ lib/colors_bloc.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CartPage extends StatelessWidget {
+  CartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var bloc = Provider.of<CartBloc>(context);
+    var cart = bloc.cart;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Shopping Cart"),
+      ),
+      body: ListView.builder(
+        itemCount: cart.length,
+        itemBuilder: (context, index) {
+          int giftIndex = cart.keys.toList()[index];
+          int? count = cart[giftIndex];
+          return ListTile(
+            leading: Container(
+              height: 50,
+              width: 50,
+            ),
+          );
+          title:
+          Text('Item Count: $count');
+          trailing:
+          RaisedButton(
+            child: Text('Clear'),
+            color: Theme.of(context).buttonColor,
+            elevation: 1.0,
+            splashColor: Colors.blueGrey,
+            onPressed: () {
+              bloc.clear(giftIndex);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CartBloc with ChangeNotifier {
+  Map<int, int> _cart = {};
+
+  Map<int, int> get cart => _cart;
+
+  void addToCart(index) {
+    if (_cart.containsKey(index)) {
+      _cart[index] = 1;
+    } else {
+      _cart[index] = 1;
+    }
+    notifyListeners();
+  }
+
+  void clear(index) {
+    if (_cart.containsKey(index)) {
+      _cart.remove(index);
+      notifyListeners();
+    }
+  }
+}
